e201d19a7b948be8f0b3ac184c7ff03fa9c408cb
Ed Crotty <ejcrotty@gmail.com>
2016-11-25 11:36:04 -0600

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -28,7 +28,10 @@ plot4 <- function(file) {
   df$Voltage <- as.numeric(as.character(df$Voltage))
   
   df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
-  
+
+##
+##  Setup 4 plots in a 2x2 matrix
+##  
   par(mfrow=c(2,2))
   
   ##PLOT 1
@@ -45,7 +48,8 @@ plot4 <- function(file) {
   #PLOT 4
   plot(df$timestamp,df$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
   
-  
+##  Copy to png
+## 
   dev.copy(png, file="plot4.png", width=480, height=480)
   dev.off()
   cat("plot4.png has been saved in", getwd())
